761b02e4a3d1e34c1d88eb27609c3547dd5243c1
Justin Alvarez <alvajus@amazon.com>
2024-08-22 19:23:46 -0400

    pre-install finch dependencies
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- scripts/setup-linux-runner.sh
+++ scripts/setup-linux-runner.sh
@@ -34,7 +34,7 @@ mkdir -p "${RUNNER_DIR}" && cd "${HOMEDIR}"
 yum upgrade
 yum group install -y "Development Tools"
 # build dependencies for packages
-yum install golang zlib-static -y
+yum install -y golang zlib-static containerd nerdctl cni-plugins
 if [ ! -z "${ADDITIONAL_PACKAGES}" ]; then
     # this sometimes fails on Amazon Linux 2, so retry if necessary
     for i in {1..2}; do yum install -y ${ADDITIONAL_PACKAGES} && break || sleep 5; done
